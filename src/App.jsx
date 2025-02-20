0c0a80de7af3f9a8709249a17baa6bdf3f799b65
Giuseppe Rizza <ilmoro86@hotmail.it>
2025-02-20 21:18:14 +0100

    [feat] Add Milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+// Creo array
+const transports = ["plane", "car", "bike", "underground", "motorbike", "ship"];
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [listTransports, setListTransports] = useState(transports);
+  const [newTransport, setNewTransport] = useState('');
+
+  const addTransport = e => {
+    event.preventDefault();
+    const updatedList = [...listTransports, newTransport];
+    setListTransports(updatedList);
+    setNewTransport("");
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={addTransport}>
+        <input type="text" value={newTransport}
+          onChange={e => { setNewTransport(e.target.value) }} />
+        <button>Submit</button>
+      </form>
+
     </>
   )
 }
